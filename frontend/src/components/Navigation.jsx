174e758dd524cc6b6dbff20eb86ef77c821fd812
raimahima22 <mahimabayingrai22@gmail.com>
2024-05-10 15:09:29 +0545

    navbar color indicator

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Tabs, Tab, Box, Button } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Vehicles from "../pages/Vehicles";
 
 const theme = createTheme({
   palette: {
@@ -12,30 +13,32 @@ const theme = createTheme({
 });
 
 function Navigation() {
-  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
+  const location = useLocation();
+  const { pathname } = location;
 
   useEffect(() => {
-    const loggedIn = localStorage.getItem("isLoggedIn");
-    const storedUsername = localStorage.getItem("username");
-    if (loggedIn && storedUsername) {
-      setIsLoggedIn(true);
-      setUsername(storedUsername);
+    console.log("pathname", pathname);
+    if (pathname) {
+      let activeIdx;
+      navItem?.forEach((item, idx) => {
+        if (item?.path === pathname) {
+          activeIdx = idx;
+        }
+      });
+
+      setValue(activeIdx);
     }
   }, []);
 
   const handleLogout = () => {
     setIsLoggedIn(false);
-    setUsername("");
-    localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("username");
-    navigate("/");
   };
 
-  const [value, setValue] = useState(0);
+  const [value, setValue] = React.useState(0);
 
-  const handleChange = (newValue) => {
+  const handleChange = (event, newValue) => {
+    console.log("clicked");
     setValue(newValue);
   };
 
@@ -46,11 +49,6 @@ function Navigation() {
     { label: "Services", path: "/services" },
     { label: "Contact", path: "/contact" },
   ];
-
-  // if user is logged in show the dashboard
-  if (isLoggedIn) {
-    navItem.push({label: "Dashboard", path: "/Dashboard"})
-  }
 
   return (
     <header
@@ -70,8 +68,8 @@ function Navigation() {
               {navItem?.map((item, idx) => (
                 <Link
                   to={item?.path}
-                  key={`nav-item-${idx}`}
-                  style={{ color: "black" }}
+                  key={`nav-tem-${idx}`}
+                  style={{ color: "black" }} // Corrected color style
                 >
                   <Tab label={item?.label} onClick={() => setValue(idx)} />
                 </Link>
@@ -82,9 +80,8 @@ function Navigation() {
 
         {isLoggedIn ? (
           <div>
-            {/* <img src="image/user-icon.jpg" alt="User Icon" /> */}
-            <span>Hello {username}</span>
-            <Button className="header-btn" onClick={handleLogout}>Logout</Button>
+            <img src="user-icon.png" alt="User Icon" />
+            <Button onClick={handleLogout}>Logout</Button>
           </div>
         ) : (
           <div className="header-btn">
