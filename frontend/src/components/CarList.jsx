f4afa7deaa18a1622a117b99db53a77855e96057
Sumit Nepal <nepalsumit30@gmail.com>
2024-05-10 12:26:38 +0545

    car edit delte and other changes

--- frontend/src/components/CarList.jsx
+++ frontend/src/components/CarList.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   Grid,
   Box,
@@ -9,26 +9,28 @@ import {
   Typography,
   Modal,
   Button,
-} from '@mui/material';
+} from "@mui/material";
 import {
   Groups as GroupsIcon,
   Settings as SettingsIcon,
   AcUnit as AcUnitIcon,
   LocalGasStation as LocalGasStationIcon,
   Luggage as LuggageIcon,
-} from '@mui/icons-material';
+} from "@mui/icons-material";
 
 function CarList() {
   const [selectedCar, setSelectedCar] = useState(null);
   const [carsData, setCarsData] = useState([]);
-  const username = 'admin';
+  const username = "admin";
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`http://localhost:8000/car/getCars/${username}`);
+      const response = await axios.get(
+        `http://localhost:8000/car/getCars/${username}`
+      );
       setCarsData(response.data);
     } catch (error) {
-      console.error('Error fetching cars data:', error);
+      console.error("Error fetching cars data:", error);
     }
   };
 
@@ -45,68 +47,79 @@ function CarList() {
   };
 
   return (
-    <Grid container spacing={3} justifyContent="center" style={{ padding: '20px' }}>
+    <Grid
+      container
+      spacing={5}
+      justifyContent="center"
+      style={{ padding: "20px" }}
+    >
       {carsData.map((car) => (
         <Grid item key={car._id} xs={12} sm={6} md={4}>
           <Card
             sx={{
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              borderRadius: '10px',
-              boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-              transition: 'transform 0.2s',
-              '&:hover': {
-                transform: 'scale(1.05)',
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              borderRadius: "10px",
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+              transition: "transform 0.2s",
+              "&:hover": {
+                transform: "scale(1.05)",
               },
             }}
             onClick={() => handleCardClick(car)}
           >
             <CardMedia
-              sx={{ width: '100%', height: 200 }}
+              sx={{ width: "100%", height: 200 }}
               image={`data:${car.picture.contentType};base64,${car.picture.data}`}
               title={car.modelName}
             />
-            <CardContent sx={{ flexGrow: 1, textAlign: 'center' }}>
+            <CardContent sx={{ flexGrow: 1, textAlign: "center" }}>
               <Typography
                 variant="h5"
                 component="div"
-                sx={{ fontWeight: 'bold', fontSize: '17px' }}
+                sx={{ fontWeight: "bold", fontSize: "17px" }}
               >
                 {car.modelName}
               </Typography>
-              <Typography variant="subtitle1" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ fontWeight: "bold" }}
+              >
                 NPR {car.price}/day
               </Typography>
-              <Box display="flex" justifyContent="space-between" paddingY="10px">
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                paddingY="10px"
+              >
                 <Box display="flex" alignItems="center">
                   <AcUnitIcon
                     sx={{
-                      color: car.haveAc ? 'black' : 'red',
+                      color: car.haveAc ? "black" : "red",
                       marginRight: 1,
                     }}
                   />
                   <Typography variant="body2">
-                    {car.haveAc ? 'Air Conditioning' : 'No AC'}
+                    {car.haveAc ? "Air Conditioning" : "No AC"}
                   </Typography>
                 </Box>
                 <Box display="flex" alignItems="center">
-                  <GroupsIcon sx={{ color: 'black', marginRight: 1 }} />
-                  <Typography variant="body2">
-                    {car.seats} seats
-                  </Typography>
+                  <GroupsIcon sx={{ color: "black", marginRight: 1 }} />
+                  <Typography variant="body2">{car.seats} seats</Typography>
                 </Box>
               </Box>
               <Box display="flex" justifyContent="space-between">
                 <Box display="flex" alignItems="center">
                   <SettingsIcon
                     sx={{
-                      color: car.system === 'auto' ? 'black' : 'red',
+                      color: car.system === "auto" ? "black" : "red",
                       marginRight: 1,
                     }}
                   />
                   <Typography variant="body2">
-                    {car.system === 'auto' ? 'Automatic' : 'Manual'}
+                    {car.system === "auto" ? "Automatic" : "Manual"}
                   </Typography>
                 </Box>
               </Box>
@@ -115,7 +128,7 @@ function CarList() {
         </Grid>
       ))}
 
-<Modal
+      <Modal
         open={!!selectedCar}
         onClose={handleCloseModal}
         aria-labelledby="modal-modal-title"
@@ -129,7 +142,7 @@ function CarList() {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: "60%",
+            width: "70%",
             bgcolor: "background.paper",
             boxShadow: 24,
             p: 4,
