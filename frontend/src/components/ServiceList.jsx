08ee5aea6eac2baa016c71a532234c8fa19ab709
rewant-the-thapa <rewantthapa4@gmail.com>
2024-05-15 21:45:41 +0545

    Added more details to services place where the user can see the available services

--- frontend/src/components/ServiceList.jsx
+++ frontend/src/components/ServiceList.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const ServiceTile = ({ title, description, imageUrl, detailLink, btnColor }) => {
   const tileStyle = {
-    backgroundColor: '#fff', // White background
+    backgroundColor: '#fff',
     padding: '20px',
     borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // subtle shadow
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
     margin: '10px',
-    display: 'flex', // Using flex to keep height consistent
-    flexDirection: 'column', // Stack elements vertically
-    justifyContent: 'space-between', // Space between elements
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    maxWidth: '200px', // Adjusted to make three tiles fit in a row
   };
 
   const imageStyle = {
-    width: '500px', // Ensuring the image is responsive
+    width: '100%', // Ensuring the image is responsive
     height: 'auto',
     marginBottom: '15px',
     borderRadius: '4px',
@@ -33,12 +34,12 @@ const ServiceTile = ({ title, description, imageUrl, detailLink, btnColor }) => 
 
 const ServiceList = () => {
   return (
-    <div className="container mt-4">
-      <div className="row justify-content-center">
+    <div className="container mt-4" style={{ margin: 'auto auto', padding: '20px' }}>
+      <div className="row justify-content-center" style={{ display: 'flex', flexDirection: 'row', flex:'300px', flexWrap:'wrap' }}>
         <ServiceTile
           title='Products'
           description='Discover products that enhance your rental experience, from GPS units to child booster seats.'
-          imageUrl = "image/products1.png"
+          imageUrl='image/products1.png'
           detailLink='/vehicles'
           btnColor='#dc3545' // Redspot red color
         />
@@ -51,7 +52,28 @@ const ServiceList = () => {
         />
         <ServiceTile
           title='Coverage Options'
-          description='Various coverage options to protect your journey.'
+          description='Car Rental Services offers a number of vehicle damage protection products that will help keep your peace of mind during your car rental trip.'
+          imageUrl='image/coverage.png'
+          detailLink='/vehicles'
+          btnColor='#dc3545'
+        />
+        <ServiceTile
+          title='Standard Fees and Charges'
+          description='Additional Charges that apply to your Car Rental Services.'
+          imageUrl='image/coverage.png'
+          detailLink='/vehicles'
+          btnColor='#dc3545'
+        />
+        <ServiceTile
+          title='Locations'
+          description='Redspot Car Rentals has locations throughout Nepal with both inside and out side valley, regional and airport offices..'
+          imageUrl='image/services.jpg'
+          detailLink='/vehicles'
+          btnColor='#dc3545'
+        />
+        <ServiceTile
+          title='Fleet'
+          description='Make your own group of cars which are the best for your own use.'
           imageUrl='image/coverage.png'
           detailLink='/vehicles'
           btnColor='#dc3545'
